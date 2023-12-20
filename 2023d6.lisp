6a481e5aa0c2e6a927e25d43b60d8b0a6e803327
Kyle Hokanson <khokanson@gmail.com>
2023-12-20 10:58:16 -0600

    p1 working with test input

--- 2023d6.lisp
+++ 2023d6.lisp
@@ -4,22 +4,39 @@
 (defconstant +day-number+ 6)
 
 (defconstant +test-input+
-  '(:T (7 15 30)
-    :D (9 40 200)))
+  '(:AT (7 15 30)
+    :RD (9 40 200)))
 
 (defconstant +full-input+
-  '(:T (56     71     79     99)
-    :D (334   1135   1350   2430)))
+  '(:AT (56     71     79     99)
+    :RD (334   1135   1350   2430)))
 
-(defun p1 ()
+(defun quad-zeroes (a b c)
+  (list (/ (+ (- b) (sqrt (- (* b b) (* 4 a c)))) (* 2 a))
+        (/ (- (- b) (sqrt (- (* b b) (* 4 a c)))) (* 2 a))))
+
+(defun beat-record (allowed-time record-distance)
   "distance = velocity * traveltime
  traveltime = allowedtime - chargetime
 velocity = chargetime
 -> distance = chargetime * (allowedtime - chargetime)
 -> distance = ct * at - ct^2
 Find ct where d > record distance
-0 > ct * at - ct^2 - d "
-  ) 
+-> rd < ct * at - ct^2
+-> -ct^2 + ct*at - rd > 0 -> a= -1 b=at c=-rd
+-> ct = (-at + sqrt (at^2 + (4 * -1 * -rd)))/(2*-1)"
+  (let ((record-chargetimes (quad-zeroes -1 allowed-time (- record-distance)))) ;;returns charge times to get the record. need the next highest int for the first and next lowest int for the second in order to beat the record
+   (list (+ (floor (first record-chargetimes)) 1)
+         (- (ceiling (second record-chargetimes)) 1))))
+
+(defun p1 (input)
+  (let ((times (getf input :AT))
+        (distances (getf input :RD))
+        winning-times)
+    (setf winning-times (mapcar #'beat-record times distances))
+    (reduce #'* (mapcar (lambda (range)
+                           (+ 1 (apply #'- (reverse range))))
+                         winning-times))))
 
 (defun p2 ()
   )
