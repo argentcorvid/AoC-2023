fc250279472f70f9be5a6098e5f3c4e7f475dc51
ArgentCorvid <khokanson@gmail.com>
2023-12-27 12:09:44 -0600

    remove dependency on 'str, re-order p2 hand classifier, needto fix fh

--- 2023d7.lisp
+++ 2023d7.lisp
@@ -1,8 +1,9 @@
 ;;;day7
-(ql:quickload '("uiop" "str"))
+(ql:quickload 'uiop)
 
 (defconstant +day-number+ 7)
-(defconstant +working-dir+ (uiop:truenamize "~/aoc_2023"))
+;;(defconstant +working-dir+ (uiop:truenamize "~/aoc_2023"))
+()
 (defconstant +input-name-template+ "2023d~dinput.txt")
 
 (defconstant +test-input+
@@ -12,19 +13,14 @@
     ("KTJJT" 220)
     ("QQQJA" 483)))
 
-(defparameter *card-value*
-  '(2 3 4 5 6 7 8 9 10 11 12 13 14))
-
 (defparameter *card-rank*
   '(#\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\T #\J #\Q #\K #\A))
 
-(defun reset ()
+(defun reset-rank ()
   (unless (find #\J *card-rank*)
     (let ((tail (member #\Q *card-rank*))
           (head (butlast *card-rank* 3)))
       (setf *card-rank* (append head '(#\J) tail)))))
-
-;;(defparameter *hand-type-stats*)
 
 (defun classify-hand (hand)
   (let* ((counts (remove 0 (mapcar (lambda (r)
@@ -36,7 +32,7 @@
                     (= 1 c)) counts)
            #x10)
           ((and (= 1 2counts)
-                1                (/= 1 3counts))
+                (/= 1 3counts))
            #x20)
           ((= 2 2counts)
            #x30)
@@ -54,29 +50,22 @@
            #x50))))
 
 (defun classify-hand-p2 (hand)
-  (let* ((counts (or (remove 0
-                             (mapcar (lambda (r)
-                                       (count r hand))
-                                     *card-rank*))
-                     '(0)))
+  (let* ((counts (mapcar (lambda (r)
+                           (count r hand))
+                         *card-rank*))
          (jcounts (count #\J hand))
          (2counts (count 2 counts))
          (3counts (count 3 counts))
-         (bestcount (or (apply #'max counts)
-                        0))
+         (bestcount (apply #'max counts))
          (j-and-b (+ jcounts bestcount)))
    ;; (break)
-    (cond ((and (= 0 jcounts)
-                (= 1 bestcount))
-           #x10) ;; high card, any joker would make a pair
-          ((and (> 2 jcounts)
-                (= 2 j-and-b)
-                (= 0 3counts))
-           #x20) ;;one pair, natural or hc with 1 joker
-          ((and (= 2 2counts)
-                (= 0 jcounts))
-           #x30) ;;two pair, any j would make make a better hand
-          
+    (cond ((< 4 j-and-b)
+           #x70) ;; 5k!
+          ((= 4 j-and-b)
+           #x60) ;; 4k
+          ((and (= 3 j-and-b)
+                (<= 1 2counts))
+           #x50) ;; fh       
           ((or (and (= 1 jcounts 2counts)
                     (= 0 3counts))
                (and (= 1 3counts)
@@ -84,18 +73,20 @@
                (and (= 1 bestcount)
                     (= 2 jcounts)))
            #x40) ;; 3k and not full house
-          ((and (= 3 j-and-b)
-                (<= 1 2counts))
-           #x50) ;; fh
-          ((= 4 j-and-b)
-           #x60) ;; 4k
-          ((< 4 j-and-b)
-           #x70) ;; 5k!
+           ((and (= 2 2counts)
+                (= 0 jcounts))
+           #x30) ;;two pair, any j would make make a better hand   
+           ((and (> 2 jcounts)
+                (= 2 j-and-b)
+                (= 0 3counts))
+           #x20) ;;one pair, natural or hc with 1 joker
+           ((and (= 0 jcounts)
+                (= 1 bestcount))
+           #x10) ;; high card, any joker would make a pair
           )))
 
 (defun card-score (card)
-  
-  (or (position card *card-rank*) -1))
+    (or (position card *card-rank*) -1))
 
 (defun hand-comp (hand1 hand2)
   (let ((h1-type (classify-hand hand1))
@@ -124,8 +115,8 @@
           (t nil))))
 
 (defun parse-input (line)
-  (let ((split-line (str:split #\Space line)))
-    (list (first split-line) (parse-integer (second split-line)))))
+  (let ((split-pos (position #\Space line)))
+    (list (subseq line 0 split-pos) (parse-integer line :start split-pos))))
 
 (defun p1 (input)
   (let ((sorted-hands (sort (copy-list input) #'hand-comp :key #'first)))
@@ -134,9 +125,10 @@
           sum (* (second h) r)))) 
 
 (defun p2 (input)
-  (delete #\J *card-rank*) ;; consider J seperately
+  (setf *card-rank* (delete #\J *card-rank*)) ;; consider J seperately
   (let* ((classed-hands (mapcar (lambda (h)
-                                  (append h (list (classify-hand-p2 (first h))))) input))
+                                  (append h (list (classify-hand-p2 (first h)))))
+                                input))
          (sorted-hands (sort classed-hands #'hand-comp-2)))
     (break)
     (loop for h in sorted-hands
@@ -144,14 +136,14 @@
           sum (* (second h) r))))
 
 (defun main ()
-  (reset)
-  (uiop:chdir +working-dir+)
-  (let* ((infile-name (format nil +input-name-template+ +day-number+))
+  (reset-rank)
+  ;;(uiop:chdir +working-dir+)
+  (let* ((infile-name (merge-pathnames +working-dir+ (format nil +input-name-template+ +day-number+)))
          (input-lines (uiop:read-file-lines infile-name))
          (data (mapcar #'parse-input input-lines)))
     (fresh-line)
     (princ "part 1: ")
-    (princ (p1 data))
+    (princ  (p1 data))
     (fresh-line)
     (princ "part 2: ")
-    (princ (p2 data))))
+    (princ  (p2 data))))
