ab8749c16d4b588f98972cb02e0f9b1a1c93a11c
ArgentCorvid <khokanson@gmail.com>
2023-12-31 13:16:33 -0600

    p1 works with test input

--- 2023d9.lisp
+++ 2023d9.lisp
@@ -6,16 +6,43 @@
 (defconstant +input-name-template+ "2023d~dinput.txt")
 
 (defconstant +test-input+
-  '())
+  '((0 3 6 9 12 15)
+    (1 3 6 10 15 21)
+    (10 13 16 21 30 45)))
 
 (defun parse-input (lines)
   )
 
-(defun p1 ()
-  ) 
+(defun p1 (data-list)
+  (reduce #'+
+          (mapcar (lambda (l) (reduce #'+ l))
+                  (mapcar (lambda (l)
+                            (mapcar #'lastc l))
+                          (mapcar #'differences data-list))))) 
 
 (defun p2 ()
   )
+
+
+(defun succ-pairs (fn lst)
+  (loop for idx from 0
+        for a = (nth idx lst)
+        for b = (nth (+ 1 idx) lst)
+        until (null b)
+        collect (funcall fn a b)))
+
+(defun differences (numbers)
+  (labels ((rec (lst acc)
+             
+             (if (every #'zerop lst)
+                 (nreverse acc)
+                 (progn
+                   (push lst acc)
+                   (rec (reverse (succ-pairs #'- (reverse lst))) acc)))))
+    (rec numbers nil)))
+
+(defun lastc (l)
+  (car (last l)))
 
 (defun main ()
   (let* ((infile-name (format nil +input-name-template+ +day-number+))
