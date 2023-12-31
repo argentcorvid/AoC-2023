f5f2e54c3ff019d99e1056262b86e39ecbc802e2
ArgentCorvid <khokanson@gmail.com>
2023-12-31 10:08:43 -0600

    day 9 initial commit

---
+++ 2023d9.lisp
@@ -0,0 +1,30 @@
+;;;day9
+(ql:quickload 'uiop)
+
+(defconstant +day-number+ 9)
+(defconstant +working-dir+ (uiop:truenamize "~/aoc_2023/"))
+(defconstant +input-name-template+ "2023d~dinput.txt")
+
+(defconstant +test-input+
+  '())
+
+(defun parse-input (lines)
+  )
+
+(defun p1 ()
+  ) 
+
+(defun p2 ()
+  )
+
+(defun main ()
+  (let* ((infile-name (format nil +input-name-template+ +day-number+))
+         (input-lines (uiop:read-file-lines infile-name))
+         (data (parse-input input-lines)))
+    (fresh-line)
+    (princ "part 1: ")
+    (princ (reduce #'+ (p1 data)))
+    (fresh-line)
+    (princ "part 2: ")
+    (fresh-line)
+    (princ (reduce #'+ (p2 data)))))
