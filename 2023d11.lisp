e4c796c2df567fcb9e5cb9403ab224d874c9d602
ArgentCorvid <khokanson@gmail.com>
2024-01-11 23:18:26 -0600

    day11p1 parsing works with test input

--- 2023d11.lisp
+++ 2023d11.lisp
@@ -1,24 +1,54 @@
 ;;;day11
-(ql:quickload :uiop)
+(ql:quickload '(uiop cl-ppcre))
 
 (defconstant +day-number+ 11)
 (defconstant +working-dir+ (uiop:truenamize "~/aoc_2023/"))
 (defconstant +input-name-template+ "2023d~dinput.txt")
 
 (defconstant +test-input+
-  "...#......"
-  ".......#.."
-  "#........."
-  ".........."
-  "......#..."
-  ".#........"
-  ".........#"
-  ".........."
-  ".......#.."
-  "#...#.....")
+  '("...#......"
+   ".......#.."
+   "#........."
+   ".........."
+   "......#..."
+   ".#........"
+   ".........#"
+   ".........."
+   ".......#.."
+   "#...#....."))
+
+(defstruct universe
+  empty-rows
+  empty-cols
+  locs)
+
 
 (defun parse-input (lines)
-  )
+  (let ((regex (ppcre:create-scanner "#"))
+        (my-universe (make-universe))
+        (line-idx 0)
+        (height (length lines))
+        (width (length (first lines))))
+    (dolist (l lines)
+      (ppcre:do-matches (ms me regex l)
+        (push (list line-idx ms) (universe-locs my-universe)))
+      (incf line-idx))
+    (setf (universe-empty-rows my-universe)
+          (set-difference
+           (loop for x from 0 below height collecting x)
+           (mapcar #'car (universe-locs my-universe))))
+    (setf (universe-empty-cols my-universe)
+          (set-difference
+           (loop for x from 0 below width collecting x)
+
+           (mapcar #'cadr (universe-locs my-universe))))
+    my-universe))
+
+(defun man-dist (pt1 pt2)
+  "find the 'manhattan' distance between 2 points"
+  (reduce #'+ (mapcar (lambda (a b)
+                        (abs (- a b)))
+                      pt1 pt2)))
 
 (defun p1 ()
   ) 
