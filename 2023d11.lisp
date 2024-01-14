e6b11306bf7e9854a17218992a0f63ed8bce8ea6
ArgentCorvid <khokanson@gmail.com>
2024-01-13 20:30:32 -0600

    d11p1 work on pt1 remove by-line input parse

--- 2023d11.lisp
+++ 2023d11.lisp
@@ -1,65 +1,41 @@
 ;;;day11
-(ql:quickload '(uiop cl-ppcre alexandria))
+(ql:quickload '(uiop alexandria))
 
 (defconstant +day-number+ 11)
 (defconstant +working-dir+ (uiop:truenamize "~/aoc_2023/"))
 (defconstant +input-name-template+ "2023d~dinput.txt")
 
 (defconstant +test-input+
-  '("...#......"
-   ".......#.."
-   "#........."
-   ".........."
-   "......#..."
-   ".#........"
-   ".........#"
-   ".........."
-   ".......#.."
-   "#...#....."))
-
-(defconstant +ti-as-string+ (format nil "~{~A~^~&~}" +test-input+))
+"...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....")
 
 (defstruct universe
   empty-rows
   empty-cols
   locs)
 
-(defun parse-input (lines)
-  (let ((regex (ppcre:create-scanner "#"))
-        (my-universe (make-universe))
-        (line-idx 0)
-        (height (length lines))
-        (width (length (first lines))))
-    (dolist (l lines)
-      (ppcre:do-matches (ms me regex l)
-        (push (list line-idx ms) (universe-locs my-universe)))
-      (incf line-idx))
-    (setf (universe-empty-rows my-universe)
-          (nset-difference
-           (loop for x from 0 below height collecting x)
-           (mapcar #'car (universe-locs my-universe))))
-    (setf (universe-empty-cols my-universe)
-          (nset-difference
-           (loop for x from 0 below width collecting x)
-
-           (mapcar #'cadr (universe-locs my-universe))))
-    my-universe))
-
 (defun read-input (stream)
   (let ((my-universe (make-universe))
         (width 0)
         (height 0))
-    (do ((ch     (read-char stream nil) (read-char stream nil))
-         (ch-idx 0                      (1+ ch-idx)))
+    (do ((ch (read-char stream nil) (read-char stream nil))
+         (ch-idx 0 (1+ ch-idx)))
         ((null ch))
       (case ch
         (#\# (push (list height ch-idx)
                    (universe-locs my-universe)))
         (#\newline (when (zerop height)
-                     (setf width (1- ch-idx)))
+                     (setf width ch-idx))
                    (incf height)
                    (setf ch-idx -1))))
-    
     (setf (universe-empty-rows my-universe)
           (nset-difference
            (loop for x from 0 below height collecting x)
@@ -67,17 +43,37 @@
     (setf (universe-empty-cols my-universe)
           (nset-difference
            (loop for x from 0 below width collecting x)
+
            (mapcar #'cadr (universe-locs my-universe))))
     my-universe))
 
 (defun man-dist (pt1 pt2)
   "find the 'manhattan' distance between 2 points"
-  (reduce #'+ (mapcar (lambda (a b)
-                        (abs (- a b)))
-                      pt1 pt2)))
+    (reduce #'+ (mapcar (lambda (a b)
+                          (abs (- a b)))
+                 pt1 pt2)))
 
-(defun p1 ()
-  ) 
+(defun elts-between (start end cands)
+  (let ((mn (min start end))
+        (mx (max start end)))
+   (remove-if (lambda (elm)
+                (or (< elm mn mx)
+                    (> elm mx mn)))
+              cands)))
+
+(defun p1 (my-universe)
+  (let (distances-list)
+    (alexandria:map-combinations
+     (lambda (pt-list)
+       (let* ((pt1 (car pt-list))
+             (pt2 (cadr pt-list))
+             (md (man-dist pt1 pt2))
+             (x-empties (length (elts-between (cadr pt1) (cadr pt2) (universe-empty-cols my-universe))))
+             (y-empties (length (elt-between (car pt1) (car pt2) (universe-empty-rows my-universe)))))
+         (push (+ md x-empties y-empties)
+               distances-list)))
+     (universe-locs my-universe) :length 2 :copy nil)
+    (reduce #'+ distances-list))) 
 
 (defun p2 ()
   )
