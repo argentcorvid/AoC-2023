4a4b70336fa73db51e07a1b531fa98d388e72f95
ArgentCorvid <khokanson@gmail.com>
2024-01-23 16:07:49 -0600

    d5 p1 - copying from a python solution - using classes - don't really understand
    https://github.com/derailed-dash/Advent-of-Code/blob/master/src/AoC_2023/Dazbo's_Advent_of_Code_2023.ipynb.

--- 2023d5.lisp
+++ 2023d5.lisp
@@ -1,12 +1,14 @@
 ;;;day5
-(ql:quickload '("uiop" "str"))
+(ql:quickload '(uiop str alexandria defclass-std))
+(import '(alexandria:iota defclass-std:defclass/std))
 
 (defconstant +day-number+ 5)
 (defconstant +working-dir+ (uiop:truenamize "~/aoc_2023/"))
 (defconstant +input-name-template+ "2023d~dinput.txt")
 
 (defconstant +test-input+
-  '("seeds: 79 14 55 13
+  (str:split #\newline
+             '("seeds: 79 14 55 13
 
 seed-to-soil map:
 50 98 2
@@ -34,10 +36,61 @@ light-to-temperature map:
 
 temperature-to-humidity map:
 0 69 1
-1 0 69"))
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4")))
+
+(defclass/std garden-map nil
+  ((src-type dest-type :std "" :type string)
+   (src-list dest-list :std ())
+   ))
+
+(defmethod add-range ((obj garden-map) src-start dest-start rng-length)
+  (push (list src-start rng-length) (src-list obj))
+  (push (list dest-start rng-length (dest-list obj)))
+  )
+
+(defmethod sort-ranges ((obj garden-map))
+  (let ((idx-list
+          (sort (iota (length (src-list obj)))
+                #'<
+                :key (lambda (idx)
+                       (first (nth idx (src-list obj)))))))
+    (setf (src-list obj) (loop for i in idx-list collecting (nth i (src-list obj))))
+    (setf (dest-list obj) (loop for i in idx-list collecting (nth i (dest-list obj))))))
+
+(defmethod get-target ((obj garden-map) source)
+  (loop for current in (src-list obj)
+        for idx from 0
+        with target = source
+        for start = (first current)
+        for end = (+ (cadr current) (first current))
+        when (and (<= start source)
+                  (< source end))
+         do (setf target (+ (- source start) (first (nth idx (dest-list obj)))))
+            (loop-finish)
+        finally (return target)))
+
+(defparameter maps '(seed-soil 
+                     soil-fert 
+                     fert-watr 
+                     watr-lite 
+                     lite-temp 
+                     temp-humd
+                     humd-locs))
 
 (defun parse-input (lines)
-  )
+  (let (seeds
+        seed-soil 
+        soil-fert 
+        fert-watr 
+        watr-lite 
+        lite-temp 
+        temp-humd
+        humd-locs)
+    ))
 
 (defun p1 ()
   ) 
