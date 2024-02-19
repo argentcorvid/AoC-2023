597ab58f76dc368ed6c3cabe30e2a8ab9f612dae
ArgentCorvid <khokanson@gmail.com>
2024-02-19 16:39:20 -0600

    d5 make classes. start methods for range

--- 2023d5.lisp
+++ 2023d5.lisp
@@ -1,12 +1,18 @@
 ;;;day5
-(ql:quickload '("uiop" "str"))
+(ql:quickload '(:alexandria
+                :defclass-std
+                :str))
+
+(import '(alexandria:assoc-value
+          alexandria:compose
+          defclass-std:class/std))
 
 (defconstant +day-number+ 5)
 (defconstant +working-dir+ (uiop:truenamize "~/aoc_2023/"))
 (defconstant +input-name-template+ "2023d~dinput.txt")
 
-(defconstant +test-input+
-  '("seeds: 79 14 55 13
+(defconstant +test-input+ (str:split #\newline  
+                           "seeds: 79 14 55 13
 
 seed-to-soil map:
 50 98 2
@@ -35,6 +41,50 @@ light-to-temperature map:
 temperature-to-humidity map:
 0 69 1
 1 0 69"))
+
+(defclass/std range nil
+  ((start end length :type fixnum :with)))
+
+(defmethod make-range-from-start-to-end ((start fixnum) (end fixnum))
+  (make-instance 'range :range-start start :range-end end :range-length (- end start)))
+
+;; methods must have same number of args :(
+;; (defmethod make-range-from-start-to-end ((s-e cons))
+;;   (if (= 2 (length s-e))
+;;       (make-range-from-start-to-end (first s-e) (second s-e))
+;;       (error "argument must be list of numbers of length 2")))
+
+(defmethod make-range-from-start-to-length ((start fixnum) (length fixnum))
+  (make-instance range :range-start start :range-end (+ start length) :range-length length))
+
+;; (defmethod make-range-from-start-to-length ((s-l cons))
+;;   (if (= 2 (length s-l))
+;;       (make-range-from-start-to-length (first s-l) (second s-l))
+;;       (error "argument must be list of numbers of length 2")))
+
+(defmethod range< ((r1 range) (r2 range))
+  (and (< (range-start r1) (range-start r2))
+       (< (range-length r1) (range-length r2))))
+
+(defmethod range-overlap? ((r1 range) (r2 range))
+  (let ((ranges (sort (list r1 r2) #'range<)))
+    (and (<= (range-start (first ranges)) (range-start (second ranges)))
+         (>= (range-end (first ranges)) (range-start (second ranges))))))
+
+(defmethod range-split ((rng range) (index fixnum))
+  (list (fste start index-1) (fste index end)))
+
+(defmethod range-split ((rng1 range) (rng2 range))
+  )
+
+(defclass/std range-mapping nil
+  ((input-range output-range :type range)))
+
+
+
+(defclass/std garden-map nil
+  ((in-name out-name :type string)
+   (mappings :type cons)))
 
 (defun parse-input (lines)
   )
