1f5080678b9c4de37f492f2cca3af68d43d4ddef
ArgentCorvid <khokanson@gmail.com>
2024-03-12 16:41:18 -0500

    d12 p1 test working - recursive without memo
    cribbed from https://qsantos.fr/2024/01/04/dynamic-programming-is-not-black-magic

--- 2023d12.lisp
+++ 2023d12.lisp
@@ -1,5 +1,9 @@
 ;;;2023 day 12
-(eval-when (:compile-toplevel :load-toplevel :execute) (ql:quickload '(:str :alexandria :defclass-std)))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload '(:str
+                  :alexandria
+                  :defclass-std
+                  :fare-memoization)))
 
 (eval-when (:compile-toplevel)
 
@@ -13,21 +17,50 @@
   (defconstant +working-dir+ (uiop:truenamize "~/aoc_2023/"))
   (defconstant +input-name-template+ "2023d~dinput.txt")
 
-  (defconstant +test-input-1+ '("???.### 1,1,3"
-                              ".??..??...?##. 1,1,3"
-                              "?#?#?#?#?#?#?#? 1,3,1,6"
+  (defconstant +test-input-1+ '("???.### 1,1,3"           ; ? unknown
+                              ".??..??...?##. 1,1,3"      ; # BROKEN
+                              "?#?#?#?#?#?#?#? 1,3,1,6"   ; . good
                               "????.#...#... 4,1,1"
                               "????.######..#####. 1,6,5"
-                              "?###???????? 3,2,1")))
+                              "?###???????? 3,2,1"))
+  (defconstant +test-results+ '(1 4 1 1 4 10)))
 
-(defclass/std spring-record nil
-  ((conditions groups :with)))
 
 (defun parse-input (lines)
- )
+  (mapcar #'(lambda (l)
+              (destructuring-bind (spring-map groups) (str:split #\space l)
+                (list spring-map (mapcar #'(lambda (n)
+                                         (parse-integer n))
+                                     (str:split #\, groups)))))
+          lines))
 
-(defun p1 ()
-  ) 
+;; "backtracking"
+;; from https://qsantos.fr/2024/01/04/dynamic-programming-is-not-black-magic
+(defun p1-backtrack-recur (condition-map groups &optional (total 0))
+  (declare (optimize (debug 3) (safety 3)))
+  (when (null groups)
+    (if (find #\# condition-map)
+        (return-from p1-recur 0)
+        (return-from p1-recur 1)))
+  (when (= 0 (length condition-map))
+    (if (null groups)
+        (return-from p1-recur 1)
+        (return-from p1-recur 0)))
+  (let ((res 0)
+        (cml (length condition-map)))
+    (when (find (schar condition-map 0) ".?")
+      (incf res (p1-recur (subseq condition-map 1) groups)))
+    (when (find (schar condition-map 0) "#?")
+      (when (and (<= (first groups) cml)
+                 (not (find #\. (subseq condition-map 0 (first groups))))
+                 (or (= (first groups) cml)
+                     (not (char= (schar condition-map (first groups))
+                                 #\#))))
+        (incf res (p1-recur (if (= cml (first groups))
+                                ""
+                                (subseq condition-map (+ 1 (first groups))))
+                            (rest groups)))))
+    res)) 
 
 (defun p2 ()
   )
